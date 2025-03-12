1c03c7df3905fed077e7406993bd4719c8d47165
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-03-12 09:04:45 +0300

    complete arithmeticlab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// 2. Create random variable that generates a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// 3. Create mod variable calculating remainder that equals 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+// 4. Create max variable finding highest number in a set (20)
+const numbers = [5, 10, 20, 15, 8];
+const max = Math.max(...numbers);
